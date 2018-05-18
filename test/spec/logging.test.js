bb0cf2c58482d93e0d578104b06f9cf3eaee426b
Thanasis Polychronakis <thanpolas@gmail.com>
2018-05-18 17:58:46 +0100

    create main logging test

---
+++ test/spec/logging.test.js
@@ -0,0 +1,53 @@
+/**
+ * @fileOverview Test normal logging.
+ */
+const sinon = require('sinon');
+
+const Logality = require('../..');
+const { sink } = require('../lib/tester.lib');
+
+describe('Normal Logging', () => {
+  let dateStub;
+  let processStub;
+  beforeEach(() => {
+    dateStub = sinon.stub(Date.prototype, 'toISOString');
+    processStub = sinon.stub(Logality.prototype, '_getPid');
+    dateStub.returns('2018-05-18T16:25:57.815Z');
+    processStub.returns(36255);
+  });
+
+  afterEach(() => {
+    dateStub.restore();
+    processStub.restore();
+  });
+
+  test('Will log expected JSON properties', (done) => {
+    const logality = new Logality({
+      appName: 'testLogality',
+      hostname: '127.0.0.1',
+      wstream: sink((chunk) => {
+        expect(chunk).toMatchSnapshot();
+        done();
+      }),
+    });
+
+    const log = logality.get();
+
+    log('info', 'hello world');
+  });
+
+  test('Will log an object in context', (done) => {
+    const logality = new Logality({
+      appName: 'testLogality',
+      hostname: '127.0.0.1',
+      wstream: sink((chunk) => {
+        expect(chunk).toMatchSnapshot();
+        done();
+      }),
+    });
+
+    const log = logality.get();
+
+    log('info', 'hello world', { custom: { a: 1, b: 2 } });
+  });
+});
