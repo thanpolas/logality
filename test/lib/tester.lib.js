46b1de06651e09f5d4ebf254eb938327b98aa9ab
Thanasis Polychronakis <thanpolas@gmail.com>
2018-05-18 17:58:25 +0100

    create sinker for logs and ISO8601 RE

--- test/lib/tester.lib.js
+++ test/lib/tester.lib.js
@@ -2,6 +2,8 @@
  * @fileOverview Main testing helper lib.
  *
  */
+const writeStream = require('flush-write-stream');
+const split = require('split2');
 
 const tester = module.exports = {};
 
@@ -16,3 +18,18 @@ tester.cooldown = function (seconds) {
     setTimeout(done, seconds);
   };
 };
+
+/**
+ * Writable stream helper for capturing logging output.
+ *
+ * @param {Function} func Callback with the logging streams.
+ * @return {Writable Stream}
+ */
+tester.sink = function (func) {
+  const result = split(JSON.parse);
+  result.pipe(writeStream.obj(func));
+  return result;
+};
+
+/** @type {Regex} Regex to test ISO 8601 Dates */
+tester.reISO8601 = /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/i;
