bde429c97e01c72915ac548a1a97b88cf62b21d7
Thanasis Polychronakis <thanpolas@gmail.com>
2018-05-21 12:57:48 +0100

    Add new surface tests for all levels

--- test/spec/surface.test.js
+++ test/spec/surface.test.js
@@ -1,8 +1,10 @@
 /**
  * @fileOverview Test the API surface, library properly exporting methods.
  */
+const sinon = require('sinon');
 
 const Logality = require('../..');
+const { sink } = require('../lib/tester.lib');
 
 describe('API Surface', () => {
   test('Logality is a function', () => {
@@ -21,5 +23,127 @@ describe('API Surface', () => {
 
     expect(typeof logality.get).toBe('function');
     expect(typeof logality.log).toBe('function');
+  });
+
+  describe('Logging Function Levels', () => {
+    let dateStub;
+    let processStub;
+
+    beforeEach(() => {
+      dateStub = sinon.stub(Date.prototype, 'toISOString');
+      processStub = sinon.stub(Logality.prototype, '_getPid');
+      dateStub.returns('2018-05-18T16:25:57.815Z');
+      processStub.returns(36255);
+    });
+
+    afterEach(() => {
+      dateStub.restore();
+      processStub.restore();
+    });
+
+    test('Testing log level: debug', (done) => {
+      const logality = new Logality({
+        appName: 'testLogality',
+        wstream: sink((chunk) => {
+          expect(chunk).toMatchSnapshot();
+          done();
+        }),
+      });
+
+      const log = logality.get();
+
+      log.debug('hello world');
+    });
+    test('Testing log level: info', (done) => {
+      const logality = new Logality({
+        appName: 'testLogality',
+        wstream: sink((chunk) => {
+          expect(chunk).toMatchSnapshot();
+          done();
+        }),
+      });
+
+      const log = logality.get();
+
+      log.info('hello world');
+    });
+    test('Testing log level: notice', (done) => {
+      const logality = new Logality({
+        appName: 'testLogality',
+        wstream: sink((chunk) => {
+          expect(chunk).toMatchSnapshot();
+          done();
+        }),
+      });
+
+      const log = logality.get();
+
+      log.notice('hello world');
+    });
+    test('Testing log level: warn', (done) => {
+      const logality = new Logality({
+        appName: 'testLogality',
+        wstream: sink((chunk) => {
+          expect(chunk).toMatchSnapshot();
+          done();
+        }),
+      });
+
+      const log = logality.get();
+
+      log.warn('hello world');
+    });
+    test('Testing log level: error', (done) => {
+      const logality = new Logality({
+        appName: 'testLogality',
+        wstream: sink((chunk) => {
+          expect(chunk).toMatchSnapshot();
+          done();
+        }),
+      });
+
+      const log = logality.get();
+
+      log.error('hello world');
+    });
+    test('Testing log level: critical', (done) => {
+      const logality = new Logality({
+        appName: 'testLogality',
+        wstream: sink((chunk) => {
+          expect(chunk).toMatchSnapshot();
+          done();
+        }),
+      });
+
+      const log = logality.get();
+
+      log.critical('hello world');
+    });
+    test('Testing log level: alert', (done) => {
+      const logality = new Logality({
+        appName: 'testLogality',
+        wstream: sink((chunk) => {
+          expect(chunk).toMatchSnapshot();
+          done();
+        }),
+      });
+
+      const log = logality.get();
+
+      log.alert('hello world');
+    });
+    test('Testing log level: emergency', (done) => {
+      const logality = new Logality({
+        appName: 'testLogality',
+        wstream: sink((chunk) => {
+          expect(chunk).toMatchSnapshot();
+          done();
+        }),
+      });
+
+      const log = logality.get();
+
+      log.emergency('hello world');
+    });
   });
 });
