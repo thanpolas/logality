faa52a516d72ea0b40bb88c35da44e9e1586253b
Thanasis Polychronakis <thanpolas@gmail.com>
2018-05-21 11:56:48 +0100

    write the error test case

--- test/spec/error-stack.test.js
+++ test/spec/error-stack.test.js
@@ -2,10 +2,45 @@
  * @fileOverview Test logging a JS error with error stack.
  */
 
+const sinon = require('sinon');
+
 const Logality = require('../..');
+const { sink } = require('../lib/tester.lib');
 
 describe('Error Stack Testing', () => {
-  test('Will properly figure out invoking function module on error', () => {
+  let dateStub;
+  let processStub;
+  beforeEach(() => {
+    dateStub = sinon.stub(Date.prototype, 'toISOString');
+    processStub = sinon.stub(Logality.prototype, '_getPid');
+    dateStub.returns('2018-05-18T16:25:57.815Z');
+    processStub.returns(36255);
+  });
 
+  afterEach(() => {
+    dateStub.restore();
+    processStub.restore();
+  });
+
+  test('Will properly figure out invoking function module on error', (done) => {
+    const logality = new Logality({
+      appName: 'testLogality',
+      hostname: '127.0.0.1',
+      wstream: sink((chunk) => {
+        expect(chunk).toMatchSnapshot();
+        done();
+      }),
+    });
+
+    const log = logality.get();
+
+    try {
+      throw new Error('An error did not occur');
+    } catch (ex) {
+
+      log('error', 'An error happened, maybe?', {
+        error: ex,
+      });
+    }
   });
 });
