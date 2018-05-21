e6615fefb01858a2ba6f821f8a4c2592ac8dfe0d
Thanasis Polychronakis <thanpolas@gmail.com>
2018-05-21 16:19:47 +0100

    refactor error test to not use snapshots, traces are hell

--- test/spec/error-stack.test.js
+++ test/spec/error-stack.test.js
@@ -11,7 +11,25 @@ describe('Error Stack Testing', () => {
     const logality = new Logality({
       appName: 'testLogality',
       wstream: sink((chunk) => {
-        expect(chunk).toMatchSnapshot();
+        expect(chunk).toHaveProperty('severity', 3);
+        expect(chunk).toHaveProperty('message', 'An error happened, maybe?');
+        expect(chunk).toHaveProperty('level', 'error');
+        expect(chunk).toHaveProperty('event');
+        expect(chunk).toHaveProperty('dt', '2018-05-18T16:25:57.815Z');
+        expect(chunk).toHaveProperty('context');
+
+        expect(chunk.event).toHaveProperty('error');
+        expect(chunk.event.error).toHaveProperty('backtrace');
+        expect(chunk.event.error).toHaveProperty('message', 'An error did not occur');
+        expect(chunk.event.error).toHaveProperty('name', 'Error');
+
+        expect(chunk.event.error.backtrace.length).toBeGreaterThan(4);
+
+        chunk.event.error.backtrace.forEach((traceItem) => {
+          expect(traceItem).toHaveProperty('file');
+          expect(traceItem).toHaveProperty('function');
+          expect(traceItem).toHaveProperty('line');
+        });
         done();
       }),
     });
@@ -21,7 +39,6 @@ describe('Error Stack Testing', () => {
     try {
       throw new Error('An error did not occur');
     } catch (ex) {
-
       log('error', 'An error happened, maybe?', {
         error: ex,
       });
