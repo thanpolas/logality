a065b3b9a63b4c33c0b0da1609efef9f3ec32027
Thanasis Polychronakis <thanpolas@gmail.com>
2020-01-29 11:10:30 +0000

    Add a test for the custom serializer

---
+++ test/spec/custom.test.js
@@ -0,0 +1,28 @@
+/**
+ * @fileOverview Test logging of custom data objects.
+ */
+const Logality = require('../..');
+const { sink, stubLogality } = require('../lib/tester.lib');
+
+const DATA = {
+  id: 10,
+  email: 'one@go.com',
+};
+
+describe('Custom Serializer', () => {
+  stubLogality();
+
+  test('Will log custom data properly', (done) => {
+    const logality = new Logality({
+      appName: 'testLogality',
+      wstream: sink((chunk) => {
+        expect(chunk).toMatchSnapshot();
+        done();
+      }),
+    });
+
+    const log = logality.get();
+
+    log('info', 'hello world', { custom: DATA });
+  });
+});
