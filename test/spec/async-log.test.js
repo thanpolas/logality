2646bb27f24cbc4d2c04ff4adcea646c7423327f
Thanasis Polychronakis <thanpolas@gmail.com>
2020-02-18 14:06:40 +0000

    test all API helpers for async as well

--- test/spec/async-log.test.js
+++ test/spec/async-log.test.js
@@ -33,6 +33,28 @@ describe('Asynchronous Logging', () => {
     await log('info', 'hello world', { custom: { a: 1, b: 2 } });
   });
 
+  test('Test API helpers with Async Logging', async () => {
+    const logality = new Logality({
+      appName: 'testLogality',
+      wstream: sink((chunk, enc, cb) => {
+        expect(chunk).toMatchSnapshot();
+        expect(enc).toEqual('utf8');
+        cb();
+      }),
+    });
+
+    const log = logality.get();
+
+    await log.debug('This is message of level: Debug');
+    await log.info('This is message of level: Info');
+    await log.notice('This is message of level: Notice');
+    await log.warn('This is message of level: warning');
+    await log.error('This is message of level: Error');
+    await log.critical('This is message of level: Critical');
+    await log.alert('This is message of level: Alert');
+    await log.emergency('This is message of level: Emergency');
+  });
+
   test('Async Logging Streamer Error Propagates', async () => {
     const logality = new Logality({
       appName: 'testLogality',
