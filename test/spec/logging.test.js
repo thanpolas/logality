abc0a003fefe762d04533b9f9613b8755ccf5865
Thanasis Polychronakis <thanpolas@gmail.com>
2020-03-22 11:22:12 +0000

    make basic spec test synch

--- test/spec/logging.test.js
+++ test/spec/logging.test.js
@@ -7,49 +7,55 @@ const { stubLogality } = require('../lib/tester.lib');
 describe('Normal Logging', () => {
   stubLogality();
 
-  test('Will log expected JSON properties', (done) => {
+  test('Will log expected JSON properties', () => {
+    let outputDone = false;
     const logality = new Logality({
       appName: 'testLogality',
       output: (logMessage) => {
         expect(logMessage).toBeString();
         expect(logMessage).toMatchSnapshot();
-        done();
+        outputDone = true;
       },
     });
 
     const log = logality.get();
 
     log('info', 'hello world');
+    expect(outputDone).toBeTrue();
   });
 
-  test('Will log a custom object in context', (done) => {
+  test('Will log a custom object in context', () => {
+    let outputDone = false;
     const logality = new Logality({
       appName: 'testLogality',
       output: (logMessage) => {
         expect(logMessage).toBeString();
         expect(logMessage).toMatchSnapshot();
-        done();
+        outputDone = true;
       },
     });
 
     const log = logality.get();
 
     log('info', 'hello world', { custom: { a: 1, b: 2 } });
+    expect(outputDone).toBeTrue();
   });
 
-  test('objectMode config will provide object as argument on output fn', (done) => {
+  test('objectMode config will provide object as argument on output fn', () => {
+    let outputDone = false;
     const logality = new Logality({
       appName: 'testLogality',
       objectMode: true,
       output: (logObject) => {
         expect(logObject).toBeObject();
         expect(logObject).toMatchSnapshot();
-        done();
+        outputDone = true;
       },
     });
 
     const log = logality.get();
 
     log('info', 'hello world', { custom: { a: 1, b: 2 } });
+    expect(outputDone).toBeTrue();
   });
 });
