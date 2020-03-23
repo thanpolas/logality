44f4df58e02d1576ba42c9a685d594d7dd0cab3b
Thanasis Polychronakis <thanpolas@gmail.com>
2020-03-23 20:26:41 +0000

    move built-in custom serializer test

--- test/serializers/custom-serializers.test.js
+++ test/serializers/custom-serializers.test.js
@@ -13,6 +13,23 @@ const DATA = {
 describe('Custom Serializers', () => {
   stubLogality();
 
+  test('The build-in custom serializer', () => {
+    let outputDone = false;
+    const logality = new Logality({
+      appName: 'testLogality',
+      output: (logMessage) => {
+        expect(logMessage).toBeObject();
+        expect(logMessage).toMatchSnapshot();
+        outputDone = true;
+      },
+    });
+
+    const log = logality.get();
+
+    log('info', 'hello world', { custom: { a: 1, b: 2 } });
+    expect(outputDone).toBeTrue();
+  });
+
   test('Will log custom serializer on context', () => {
     const serializers = {
       zit: (data) => {
