456c1b8991758900e631dc8a5cc5f2cd5d1e44e7
Thanasis Polychronakis <thanpolas@gmail.com>
2020-03-29 13:56:20 +0100

    more extensive middleware use() tests and snapshots

--- test/spec/use.test.js
+++ test/spec/use.test.js
@@ -10,14 +10,75 @@ describe('Middleware interface', () => {
   test('Middleware will receive log context', () => {
     const logality = new Logality();
 
-    let outputDone = false;
-    logality.use((logContext) => {
-      expect(logContext).toBeObject();
-      expect(logContext).toMatchSnapshot();
-      outputDone = true;
-    });
+    const middStub = jest.fn();
+    logality.use(middStub);
 
-    logality.get().log('hello world');
-    expect(outputDone).toBeTrue();
+    logality.get()('info', 'hello world');
+
+    expect(middStub).toHaveBeenCalledTimes(1);
+    const callArgs = middStub.mock.calls[0];
+
+    expect(callArgs[0]).toBeObject();
+    expect(callArgs[0]).toMatchSnapshot();
+    expect(callArgs[1]).toBeFalse();
+  });
+
+  test('Async Middleware will receive log context', async () => {
+    const logality = new Logality({ async: true });
+
+    const middStub = jest.fn(async () => {
+      await cooldown();
+    });
+    logality.use(middStub);
+
+    logality.get()('info', 'hello async world');
+
+    expect(middStub).toHaveBeenCalledTimes(1);
+    const callArgs = middStub.mock.calls[0];
+
+    expect(callArgs[0]).toBeObject();
+    expect(callArgs[0]).toMatchSnapshot();
+    expect(callArgs[1]).toBeFalse();
+  });
+
+  test('Middleware return value will be ignored', () => {
+    const logality = new Logality();
+
+    const middStub = jest.fn(() => 'bogus');
+    logality.use(middStub);
+
+    logality.get()('info', 'hello bogus world');
+
+    expect(middStub).toHaveBeenCalledTimes(1);
+    const callArgs = middStub.mock.calls[0];
+
+    expect(callArgs[0]).toBeObject();
+    expect(callArgs[0]).toMatchSnapshot();
+    expect(callArgs[1]).toBeFalse();
+  });
+
+  test('Middleware can mutate the Log Context', () => {
+    const output = jest.fn();
+    const logality = new Logality({ output });
+
+    const middStub = jest.fn((logContext) => {
+      logContext.middleware = true;
+      logContext.middleware2 = true;
+    });
+    logality.use(middStub);
+
+    logality.get()('info', 'hello bogus world');
+
+    expect(middStub).toHaveBeenCalledTimes(1);
+    const callArgs = middStub.mock.calls[0];
+
+    expect(callArgs[0]).toBeObject();
+    expect(callArgs[0]).toMatchSnapshot();
+    expect(callArgs[1]).toBeFalse();
+
+    expect(output).toHaveBeenCalledTimes(1);
+    const logContext = output.mock.calls[0][0];
+    expect(logContext.middleware).toBeTrue();
+    expect(logContext.middleware2).toBeTrue();
   });
 });
